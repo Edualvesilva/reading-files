5d38019a7063fd8e98e65b55a749beffbb329247
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2025-01-23 00:07:54 -0300

    Writing in files

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 
-employee_file = open("employees.txt","r")
-for employee in employee_file.readlines():
-    print(employee)
+# employee_file = open("employees.txt","r")
+# for employee in employee_file.readlines():
+  #  print(employee)
 
+# employee_file.close()
+
+
+#### Writing to files
+
+employee_file = open("index.html","w")
+employee_file.write("<p>HTML</p>")
 employee_file.close()
